27b3570cea8ff4c3542d9d78f932c5d41c99e51a
DantheMobbs <dan@danmobbs.co>
2023-02-26 12:40:02 +0000

    done

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP Lesson 1</title>
+</head>
+<body>
+    
+<form>
+    <input type="text" name="num1" placeholder="Number 1">
+    <input type="text" name="num2" placeholder="Number 2">
+    <select name="operator">
+        <option>None</option>
+        <option>Add</option>
+        <option>Subtract</option>
+        <option>Multiply</option>
+        <option>Divide</option>
+    </select>
+    <button type="submit" name="submit" value="submit">Calculate</button>
+</form>
+<p>The answer is: </p>
+
+<!-- PHP code starts -->
+<?php
+
+if (isset($_GET['submit'])) {
+    $result1 = $_GET['num1'];
+    $result2 = $_GET['num2'];
+    $operator = $_GET['operator'];
+    switch ($operator) {
+        case "None":
+            echo "Please select a method!";
+        break;
+        case "Add":
+            echo $result1 + $result2;
+        break;
+        case "Subtract":
+            echo $result1 - $result2;
+        break;
+        case "Multiply":
+            echo $result1 * $result2;
+        break;
+        case "Divide":
+            echo $result1 / $result2;
+        break;  
+    }
+    
+}
+
+?>
+    
+
+</body>
+</html>
